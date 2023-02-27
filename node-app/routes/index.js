c57e42c89e7bf40b1629dfd133b3d9862a7d01e4
Matt Spriggs <mspriggs1@cnm.edu>
2023-02-28 11:06:44 +1300

    Starting the work on controllers

--- node-app/routes/index.js
+++ node-app/routes/index.js
@@ -1,26 +1,7 @@
 const express = require("express");
 const router = express.Router();
-
+const storeController = require("../controllers/storeController");
 // Do work here
-router.get("/", (req, res) => {
-  const wes = { name: "Wes", age: 100, cool: true };
-  //Can only send one piece of data at time
-  // res.send('Hey! It works!');
-  // res.json(wes);
-  // res.send(req.query.name);
-  //res.send(req.query); //will echo back the parameters in the URL such as /?name=kait&age=100&cool=true in json format
-  //Jade changed to Pug or .pug
-  //the below will pull the information from the request which is sent via the render and what is placed in the URL
-  res.render("hello", {
-    name: "wes",
-    dog: req.query.dog,
-    title: "I love food",
-  });
-});
-
-router.get("/reverse/:name", (req, res) => {
-  const reverse = [...req.params.name].reverse().join("");
-  res.send(reverse);
-});
+router.get("/", storeController.homePage);
 
 module.exports = router;
