6cb7857ed40adc87acd3bb8fe56736b9559cc710
Matt Spriggs <mspriggs1@cnm.edu>
2023-02-28 12:46:02 +1300

    Geeting the store edit form working to add a store to the DB

--- node-app/routes/index.js
+++ node-app/routes/index.js
@@ -5,5 +5,5 @@ const storeController = require("../controllers/storeController");
 // router.get("/", storeController.myMiddleware, storeController.homePage);
 router.get("/", storeController.homePage);
 router.get("/add", storeController.addStore);
-
+router.post("/add", storeController.createStore);
 module.exports = router;
