0c346980e54109806be368c71f157e4db732a10d
Matt Spriggs <mspriggs1@cnm.edu>
2023-02-27 16:04:12 +1300

    Working with data and routing

--- node-app/routes/index.js
+++ node-app/routes/index.js
@@ -1,9 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+router.get("/", (req, res) => {
+  const wes = { name: "Wes", age: 100, cool: true };
+  //Can only send one piece of data at time
+  // res.send('Hey! It works!');
+  // res.json(wes);
+  res.send(req.query.name);
 });
 
 module.exports = router;
