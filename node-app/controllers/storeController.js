80b3cee0811524b322b83536be573d24c3430cb3
Matt Spriggs <mspriggs1@cnm.edu>
2023-02-28 11:44:00 +1300

    Setting up the models for MongoDB

--- node-app/controllers/storeController.js
+++ node-app/controllers/storeController.js
@@ -1,3 +1,11 @@
+// exports.myMiddleware = (req, res, next) => {
+//   req.name = "Matt";
+//   // res.cookie("name", "Matt is cool", { maxAge: 9000000 });Sets a cookie if needed
+//   // if (req.name === "Matt") { To throw an error
+//   next(); //passes it to the next middleware function in index.js
+// };
+
 exports.homePage = (req, res) => {
+  console.log(req.name);
   res.render("index");
 };
