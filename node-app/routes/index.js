a7404eca5e4e484b3a13d9a6422eefbc2c96f20b
Matt Spriggs <mspriggs1@cnm.edu>
2023-02-27 16:29:55 +1300

    Working with parameters in the URL and pulling to the response

--- node-app/routes/index.js
+++ node-app/routes/index.js
@@ -7,7 +7,12 @@ router.get("/", (req, res) => {
   //Can only send one piece of data at time
   // res.send('Hey! It works!');
   // res.json(wes);
-  res.send(req.query.name);
+  // res.send(req.query.name);
+  res.send(req.query); //will echo back the parameters in the URL such as /?name=kait&age=100&cool=true in json format
+});
+
+router.get("/reverse/:name", (req, res) => {
+  res.send(req.params.name);
 });
 
 module.exports = router;
