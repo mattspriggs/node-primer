291f913275c3dc78c1f6e482daa45841097cfa55
Matt Spriggs <mspriggs1@cnm.edu>
2023-03-02 09:34:52 +1300

    Working on promise structure

--- node-app/controllers/storeController.js
+++ node-app/controllers/storeController.js
@@ -4,6 +4,8 @@
 //   // if (req.name === "Matt") { To throw an error
 //   next(); //passes it to the next middleware function in index.js
 // };
+const mongoose = require("mongoose");
+const Store = mongoose.model("Store");
 
 exports.homePage = (req, res) => {
   console.log(req.name);
@@ -14,6 +16,7 @@ exports.addStore = (req, res) => {
   res.render("editStore", { title: "Add Store" });
 };
 
-exports.createStore = (req, res) => {
-  res.json(req.body);
+exports.createStore = async (req, res) => {
+  const store = new Store(req.body);
+  await store.save();
 };
