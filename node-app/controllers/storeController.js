c0ef8dd5c3a1882e2405ce81b02d49ba78c91df4
Matt Spriggs <mspriggs1@cnm.edu>
2023-03-02 15:54:18 +1300

    Refactored the create store code and added redirect

--- node-app/controllers/storeController.js
+++ node-app/controllers/storeController.js
@@ -8,11 +8,6 @@ const mongoose = require("mongoose");
 const Store = mongoose.model("Store");
 
 exports.homePage = (req, res) => {
-  console.log(req.name);
-  req.flash("error", "Something Happened");
-  req.flash("info", "Something Happened");
-  req.flash("warning", "Something Happened");
-  req.flash("success", "Something Happened");
   res.render("index");
 };
 
@@ -21,11 +16,10 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save();
+  const store = await new Store(req.body).save();
   req.flash(
     "success",
     `Successfully Created ${store.name}. Care to leave a review?`
   );
-  res.redirect("/");
+  res.redirect(`/store/${store.slug}`);
 };
