6d0df1b9c94e3dabe92e94c08a63d4f2e97fd18d
Matt Spriggs <mspriggs1@cnm.edu>
2023-03-07 11:37:46 +1300

    Adding auto-complete for address and lat and lng

--- node-app/public/javascripts/modules/autocomplete.js
+++ node-app/public/javascripts/modules/autocomplete.js
@@ -1,6 +1,11 @@
 function autocomplete(input, latInput, lngInput) {
   if (!input) return; //skip this function from running if there is no input
   const dropdown = new google.maps.places.Autocomplete(input);
+
+  dropdown.addListener("place_changes", () => {
+    const place = dropdown.getPlace();
+    console.log(place);
+  });
 }
 
 export default autocomplete;
