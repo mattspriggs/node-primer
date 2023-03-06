7bad7b2213c7f41193d5d1eda2bba469c49a6e54
Matt Spriggs <mspriggs1@cnm.edu>
2023-03-07 12:02:14 +1300

    Added the auto complete for Google Maps

--- node-app/public/dist/App.bundle.js
+++ node-app/public/dist/App.bundle.js
@@ -13,6 +13,15 @@ function autocomplete(input, latInput, lngInput) {
   if (!input) return; //skip this function from running if there is no input
 
   var dropdown = new google.maps.places.Autocomplete(input);
+  dropdown.addListener("place_changed", function () {
+    var place = dropdown.getPlace();
+    latInput.value = place.geometry.location.lat();
+    lngInput.value = place.geometry.location.lng();
+  }); //If someone hits enter on the address field, don't submit form
+
+  input.on("keydown", function (e) {
+    if (e.keyCode === 13) e.preventDefault();
+  });
 }
 
 /* harmony default export */ __webpack_exports__["default"] = (autocomplete);
--- node-app/public/javascripts/modules/autocomplete.js
+++ node-app/public/javascripts/modules/autocomplete.js
@@ -2,9 +2,14 @@ function autocomplete(input, latInput, lngInput) {
   if (!input) return; //skip this function from running if there is no input
   const dropdown = new google.maps.places.Autocomplete(input);
 
-  dropdown.addListener("place_changes", () => {
+  dropdown.addListener("place_changed", () => {
     const place = dropdown.getPlace();
-    console.log(place);
+    latInput.value = place.geometry.location.lat();
+    lngInput.value = place.geometry.location.lng();
+  });
+  //If someone hits enter on the address field, don't submit form
+  input.on("keydown", (e) => {
+    if (e.keyCode === 13) e.preventDefault();
   });
 }
 
