6a66224149e660142c3da8b2455be35443d2d742
Matt Spriggs <mspriggs1@cnm.edu>
2023-03-09 14:19:02 +1300

    Register form and route created

--- node-app/routes/index.js
+++ node-app/routes/index.js
@@ -29,5 +29,6 @@ router.get("/tags", catchErrors(storeController.getStoresByTag));
 router.get("/tags/:tag", catchErrors(storeController.getStoresByTag));
 
 router.get("/login", userController.loginForm);
+router.get("/register", userController.registerForm);
 
 module.exports = router;
