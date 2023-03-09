9b6c029b742fe93f946041f1e6b0173cfdfc4511
Matt Spriggs <mspriggs1@cnm.edu>
2023-03-09 13:03:22 +1300

    Added stores to the full tag page

--- node-app/controllers/storeController.js
+++ node-app/controllers/storeController.js
@@ -96,8 +96,9 @@ exports.getStoreBySlug = async (req, res, next) => {
 
 exports.getStoresByTag = async (req, res) => {
   const tag = req.params.tag;
+  const tagQuery = tag || { $exists: true };
   const tagsPromise = Store.getTagsList();
-  const storesPromise = Store.find({ tags: tag });
+  const storesPromise = Store.find({ tags: tagQuery });
   const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);
   res.render("tag", { tags, title: "Tags", tag, stores });
 };
