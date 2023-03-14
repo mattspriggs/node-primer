838ed83683c7cfc18f6e1f7837fd69f3b95530b8
Matt Spriggs <mspriggs1@cnm.edu>
2023-03-15 11:19:37 +1300

    Resolved the rendering issue for the hearts

--- node-app/controllers/storeController.js
+++ node-app/controllers/storeController.js
@@ -6,6 +6,7 @@
 // };
 const mongoose = require("mongoose");
 const Store = mongoose.model("Store");
+const User = mongoose.model("User");
 const multer = require("multer");
 const jimp = require("jimp");
 const uuid = require("uuid");
